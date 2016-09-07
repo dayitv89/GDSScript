9ec095ced06876a0acc57df909e12ab592c3fa39
GauravDS <gaurav.sharma@punchh.com>
2016-09-08 00:44:02 +0530

    All fine code

--- FindVC/FindVC.py
+++ FindVC/FindVC.py
@@ -13,8 +13,96 @@ else:
     INFO = "[*] "
     WARN = "[!] "
 
+class OCHeader(object):
+	''' Represents an objective-c header file and it's methods, etc '''
+
+	def __init__(self, file_path, lineNo, line, verbose=False):
+		self.file_path = os.path.abspath(file_path)
+		self.file_name = os.path.basename(self.file_path)
+		self.lineNo = lineNo
+		self.line = line
+		self._class_name = None
+		self._super_class_name = None
+		self._pretty_class_name = None
+		self.all_ok = False
+		self.verbose = verbose
+		# call all this
+		self.class_name()
+		self.super_class_name()
+		self.pretty_class_name()
+
+	def __str__(self): return str(self.class_name()) + " : " +str(self.super_class_name()) + " = " + str(self.pretty_class_name())
+
+	def __repr__(self): return str(self.class_name()) + " : " +str(self.super_class_name()) + " = " + str(self.pretty_class_name()) + " line " + self.line
+
+	def toCSV(self): return str(self.class_name()) + "," +str(self.super_class_name()) + "," + str(self.pretty_class_name())
+
+	def class_name(self):
+		''' Get class name from source code '''
+		if self._class_name is not None:
+			return self._class_name
+		else:
+			if self.line.startswith('@interface'):
+				class_name = self.line.split('@interface')[1].lstrip().split(' ')[0]
+				if self.verbose: print(INFO + "Found class: %s" % class_name)
+				self._class_name = class_name.strip()
+				self.all_ok = True
+				return self._class_name
+
+	def super_class_name(self):
+		''' Get super class name from source code '''
+		if self._super_class_name is not None:
+			return self._super_class_name
+		else:
+			if self.line.startswith('@interface'):
+				try: 
+					super_class_name = self.line.split(':')[1]
+					super_class_name = re.split('<|\n| ', super_class_name.lstrip())[0]
+					if self.verbose: print(INFO + "Found superclass: %s" % super_class_name)
+					self._super_class_name = super_class_name.strip()
+					if self._super_class_name not in self.superExcept:
+						self.all_ok = True
+					else:
+						self.all_ok = False
+					return self._super_class_name
+				except IndexError as error:
+					pass
+
+	def pretty_class_name(self):
+		''' Get pretty name of class name '''
+		if self._pretty_class_name is not None:
+			return self._pretty_class_name
+		else:
+			## main logic
+			if self.all_ok:
+				self._pretty_class_name = self.convertName(self.class_name())
+				return self._pretty_class_name
+			else:
+				self._pretty_class_name = "class name not ok " + str(self._class_name)
+			return self._pretty_class_name
 
 
+### HELPER 
+	keywordsReplace = { 
+		"View Controller" : "Screen",
+		"view Controller" : "Screen",
+		"view controller" : "Screen",
+		"View" : "VIEW***",
+		"view" : "VIEW***"
+	}
+
+	superExcept = ["NSObject", "UIView", "UIViewController", "UITabBarController", "SFSafariViewController", "JSONModel"]
+
+	def convertName(self, name):
+		## camel to space sepecrate code 
+		s1 = re.sub('(.)([A-Z][a-z]+)', r'\1 \2', name)
+		s2 = re.sub('([a-z0-9])([A-Z])', r'\1 \2', s1)
+		## convert words
+		for key in self.keywordsReplace:
+			s2 = s2.replace(key, self.keywordsReplace[key])
+		return s2
+
+#### MAIN LOGIC 
 files = []
 dirs = [sys.argv[1]]
 ocfiles = []
@@ -29,7 +117,6 @@ def listAll(path, ext):
 		break
 
 def printDetails(filePath):
-	val = ''
 	lineNo = 0
 	for line in open(filePath, 'r'):
 		line = line.strip()
@@ -37,74 +124,7 @@ def printDetails(filePath):
 		lineNo += 1
 		if line.startswith("@interface"):
 			oc = OCHeader(filePath, lineNo, line)
-			if oc.all_ok:	ocfiles.append(oc)
-			val += 'Line: '+str(lineNo)+' => '+line+'\n'
-	# print val
-
-class OCHeader(object):
-	''' Represents an objective-c header file and it's methods, etc '''
-
-	def __init__(self, file_path, lineNo, line, verbose=False):
-		self.file_path = os.path.abspath(file_path)
-		self.file_name = os.path.basename(self.file_path)
-		self.lineNo = lineNo
-		self.line = line
-		self._class_name = None
-		self._super_class_name = None
-		self.all_ok = True
-		self._pretty_class_name = None
-		self.verbose = verbose
-		self.class_name()
-		self.super_class_name()
-		self.pretty_class_name()
-
-	def __str__(self):
-		return str(self.class_name()) + " : " +str(self.super_class_name()) + " = " + str(self.pretty_class_name())
-
-	def __repr__(self):
-		return str(self.class_name()) + " : " +str(self.super_class_name()) + " = " + str(self.pretty_class_name())
-
-	def class_name(self):
-		''' Get class name from source code '''
-		if self._class_name is not None:
-			return self._class_name
-		else:
-			if self.line.startswith('@interface'):
-				class_name = self.line.split(' ')[1]
-				if self.verbose: print(INFO + "Found class: %s" % class_name)
-				self._class_name = class_name.strip()
-				self._all_ok = True
-				return self._class_name
-
-	def super_class_name(self):
-		''' Get super class name from source code '''
-		if self._super_class_name is not None:
-			return self._super_class_name
-		else:
-			if self.line.startswith('@interface'):
-				try: 
-					super_class_name = self.line.split(':')[1]
-					super_class_name = re.split('<|\n| ', super_class_name.lstrip())[0]
-					if self.verbose: print(INFO + "Found superclass: %s" % super_class_name)
-					self._super_class_name = super_class_name.strip()
-					self._all_ok = True
-					return self._super_class_name
-				except IndexError as error:
-					pass
-
-	def pretty_class_name(self):
-		''' Get pretty name of class name '''
-		if self._pretty_class_name is not None:
-			return self._pretty_class_name
-		else:
-			## main logic
-			# temp logic
-			if self.all_ok:
-				self._pretty_class_name = self.class_name()
-				return self._pretty_class_name
-			return "class name not found"
-	
-
+			if oc.all_ok: ocfiles.append(oc)
 
 ### finds all directories recursively into `dirs`; and finds only .h files into `files`
 for a in dirs: listAll(a, ".h") 
