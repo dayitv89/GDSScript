ef3288304998fae60b62fda7acc8dc686ca63536
GauravDS <gaurav.sharma@punchh.com>
2016-09-07 22:46:25 +0530

    clean up

--- FindVC/FindVC.py
+++ FindVC/FindVC.py
@@ -6,24 +6,39 @@ from ioshooker import *
 files = []
 dirs = [sys.argv[1]]
 
+linesExcept = ("//", "#")
+lineInclude = ("@interface")
+
 def listAll(path, ext):
     for (dirpath, dirnames, filenames) in walk(path):
         files.extend([path+'/'+f for f in filenames if f.endswith(ext)])
         dirs.extend([path+'/'+f for f in dirnames])
         break
 
-def parseHeader(filePath, outputFileName = "out.txt"):
-	try:
-	    objc = ObjcHeader(filePath, outputFileName)
-	    print objc.class_name + " : " + objc.super_class_name
-	except ValueError as error:
-	    pass
+def printDetails(filePath):
+    val = ''
+    lineNo = 0
+    sep = 0
+    for line in open(filePath, 'r'):
+    	line = line.strip()
+    	if line.startswith(linesExcept): continue
+        lineNo += 1
+        if line.startswith("@interface"):
+        	if sep == 0:
+        		val += "\n\n--------------------------------------------------------------\n"
+        		val += filePath
+        		val += "\n--------------------------------------------------------------\n"
+        		sep = 1
+        	val += 'Line: '+str(lineNo)+' => '+line+'\n'
+    if sep == 1: val += "\n--------------------------------------------------------------\n"
+    print val
+    
 
 ### finds all directories recursively into `dirs`; and finds only .h files into `files`
 for a in dirs: listAll(a, ".h") 
 
 # pprint(files)
-for fl in files: parseHeader(fl)
+for fl in files: printDetails(fl)
 
 
 
