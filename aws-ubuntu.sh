9af600aeba64be4fa8392b47461271c6ee3f1ad1
GauravDS <er.gauravds@gmail.com>
2017-10-10 16:54:32 +0530

    aws updates

--- aws-ubuntu.sh
+++ aws-ubuntu.sh
@@ -22,3 +22,48 @@ sudo apt-get install -y build-essential
 node -v # 8.x
 npm -v  # 5.3 or 5.x
 
+# install express and PM2 
+mkdir ~/www
+cd ~/www
+touch package.json < {}
+npm i express
+sudo npm i pm2 -g
+
+# write some code on app.js
+# $ nano ~/www/app.js
+#	var express = require('express');
+#	var app = express();
+#	app.get('/', function(req, res){
+#   		res.send("Hello World!");
+#	});
+#	app.listen(3000, 'private_ip_address');
+
+# run pm2 for node server
+# $ pm2 start ~/www/app.js
+
+# start server and basic routing in nginx 
+# https://medium.com/@utkarsh_verma/configure-nginx-as-a-web-server-and-reverse-proxy-for-nodejs-application-on-aws-ubuntu-16-04-server-872922e21d38
+
+# put this config in nginx to node server
+# sudo rm /etc/nginx/sites-available/default
+# sudo nano /etc/nginx/sites-available/default
+#	server {
+#    		listen 80;
+#    		server_name your_domain.com;
+#    		location / {
+#        		proxy_pass http://private_ip_address:3000;
+#        		proxy_http_version 1.1;
+#        		proxy_set_header Upgrade $http_upgrade;
+#        		proxy_set_header Connection 'upgrade';
+#        		proxy_set_header Host $host;
+#        		proxy_cache_bypass $http_upgrade;
+#     		}
+#	} 
+
+# restart nginx after everything
+# $ sudo nginx -t
+# $ sudo /etc/init.d/nginx reload
+
+
+# reload nginx after config
+# sudo /etc/init.d/nginx reload
